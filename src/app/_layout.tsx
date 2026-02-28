922e176b439a1a3e3643f1d43510a7ada2cf0f6f
Eric Mignardi <mignardi.e@gmail.com>
2026-02-28 13:37:19 -0500

    Added Clerk as Auth Provider to the Root Layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,15 @@
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Stack } from "expo-router";
 import "../../global.css";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)"></Stack.Screen>
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="(auth)" />
+      </Stack>
+    </ClerkProvider>
   );
 }
