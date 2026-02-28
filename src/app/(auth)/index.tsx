76e0d0812dbdb9daf71e865df01a29ab0f013fa9
Eric Mignardi <mignardi.e@gmail.com>
2026-02-28 13:45:03 -0500

    Added index entry file for AuthLayout

---
+++ src/app/(auth)/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const AuthIndex = () => {
+  return (
+    <View>
+      <Text>AuthIndex</Text>
+    </View>
+  );
+};
+
+export default AuthIndex;
